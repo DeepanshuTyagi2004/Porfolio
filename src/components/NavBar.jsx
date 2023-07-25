2199ec8a2ed2ed936643b5884d3ae5db1079803a
Deepanshu <113329037+Kirito04@users.noreply.github.com>
2023-07-25 21:05:17 +0530

    Logo added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,19 @@
+import {logo} from '../assets';
+
 function NavBar({}) {
   return (
-    <div className="flex items-center justify-between bg-[#272822] px-6 py-4"></div>
+    <div className="flex items-center bg-[#272822] px-6 py-4 w-full">
+      <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
+        <div className="flex justify-center items-center">
+          <img
+            src={logo}
+            alt="logo"
+            className="w-11 h-9 rounded-full object-contain"
+          />
+          <p className="ml-3 text-white font-bold text-18px]">Deepanshu</p>
+        </div>
+      </div>
+    </div>
   )
 }
 export default NavBar
